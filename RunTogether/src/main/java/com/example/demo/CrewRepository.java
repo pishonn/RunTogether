00e238f3eb2f5c6bb49e035177b5361c77abda24
pishonn <pishon@bu.ac.kr>
2024-06-15 06:57:07 +0900

    final

--- RunTogether/src/main/java/com/example/demo/CrewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CrewRepository extends JpaRepository<Crew, Long> {
-    boolean existsByName(String name);
-}
